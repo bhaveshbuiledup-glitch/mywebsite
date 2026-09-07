b6ad1d64d81fccf16d9b689e95065224d4d109b1
Your Name <your@email.com>
2026-09-07 16:19:31 +0530

    Fix Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
+// import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: '/mywebsite/',
+//   plugins: [react()],
+// })
+
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/mywebsite/',
   plugins: [react()],
 })
